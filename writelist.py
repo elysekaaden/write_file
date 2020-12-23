a75d92bcd7f14c93442b4d5b89bffc4c10cd7079
elysekaaden <70551338+elysekaaden@users.noreply.github.com>
2020-12-23 15:19:37 +1100

    Update writelist.py

--- writelist.py
+++ writelist.py
@@ -1,13 +1,6 @@
 data = [1, 3, 5, 7, 9] # 清單中裝著一些整數
 # 請開始寫"寫入檔案"的程式碼
 
-num = int(input('Please enter random number: '))
-data.append(num)
-print(num)
-print(data)
-print(data[0])
-print(data[1])
-
 with open('test.csv', 'w') as f:
 	f.write(
 		str(data[0]) + '\n' + 
